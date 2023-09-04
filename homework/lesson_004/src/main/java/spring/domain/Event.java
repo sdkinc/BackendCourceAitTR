81d5b193b500c87828a6e2806efd1dd5ea85d815
Oleksandr Zhurba <starkyzh@gmail.com>
2023-09-04 22:59:30 +0200

    feat: solve HW lesson_004

---
+++ homework/lesson_004/src/main/java/spring/domain/Event.java
@@ -0,0 +1,40 @@
+package spring.domain;
+
+public class Event {
+    private String name;
+    private String city;
+    private double price;
+
+    public Event() {
+    }
+
+    public Event(String name, String city, double price) {
+        this.name = name;
+        this.city = city;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
