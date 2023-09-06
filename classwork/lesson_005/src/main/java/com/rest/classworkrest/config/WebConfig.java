19cc4a5879973533c394d19771566432b13b2693
Oleksandr Zhurba <starkyzh@gmail.com>
2023-09-06 07:26:01 +0200

    feat: add classwork lesson_005

---
+++ classwork/lesson_005/src/main/java/com/rest/classworkrest/config/WebConfig.java
@@ -0,0 +1,2 @@
+package com.rest.classworkrest.config;public class WebConfig {
+}
---
+++ classwork/lesson_005/src/main/java/com/rest/classworkrest/controller/EventController.java
@@ -0,0 +1,9 @@
+package com.rest.classworkrest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class EventController {
+
+}
